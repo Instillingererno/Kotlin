45155b46aee661160a775e28f4e04c34e3cfdb5f
Sveinung Øverland <sveinungskole@gmail.com>
2018-08-27 13:43:42 +0200

    oioioioioioioioioioioiooio

--- Oppgaver/src/algoritmerOgDatastrukturer/ovelse1_aksje.kt
+++ Oppgaver/src/algoritmerOgDatastrukturer/ovelse1_aksje.kt
@@ -5,11 +5,29 @@ import com.xeiam.xchart.SwingWrapper
 import java.util.stream.IntStream
 
 fun main(args: Array<String>) {
-    val stockChanges = IntStream.generate { Math.round(10 - Math.random() * 20).toInt() }.limit(20).toArray()  //arrayOf(-6,4,0,-3,-9,0,4,1,-2) 4-7   arrayOf(-1,3,-9,2,2,-1,2,-1,-5)
+    val stockChanges = intArrayOf(-1,3,-9,2,2,-1,2,-1,-5) // arrayOf(-6,4,0,-3,-9,0,4,1,-2) 4-7  IntStream.generate { Math.round(10 - Math.random() * 20).toInt() }.limit(40).toArray()
     val stockChange = StockChange(stockChanges)
 
-    println("For stock changes $stockChanges the best day for buying is ${stockChange.bestBuyDate} and best selling day is ${stockChange.bestSellDate}")
+    var runder = 0
+    val startTid = System.currentTimeMillis()
+    do {
+        runder++
+        val temp = StockChange(stockChanges)
+    } while (System.currentTimeMillis() - startTid < 1000)
+    val sluttTid = System.currentTimeMillis()
 
+    println("""
+        Målte runder var: $runder på ${sluttTid - startTid}ms
+        Som gir en estimert tid på ${(sluttTid-startTid).toDouble() / runder.toDouble()}ms per runde
+        n er i denne gjennomkjøringen lik: ${stockChanges.size}
+
+        kompleksiteten burde være O(n)
+        n lik 40 har gitt tiden 10.4360 E-5 ms
+        n lik 30 har gitt tiden  7.3424 E-5 ms
+        n lik 20 har gitt tiden  5.2349 E-5 ms
+        n lik 10 har gitt tiden  3.8327 E-5 ms
+
+    """.trimIndent())
 
     showGraph(data = DoubleArray(stockChanges.size) { stockChange.dailyPrice[it].toDouble() }, funName = "Best days = [${stockChange.bestBuyDate}  ${stockChange.bestSellDate}]" )
 
@@ -18,29 +36,6 @@ fun main(args: Array<String>) {
 fun showGraph(data: DoubleArray, title: String = "Aksjepris", xTitle: String = "Dag", yTitle: String = "Pris", funName: String = "Data") {
     SwingWrapper(QuickChart.getChart(title, xTitle, yTitle, funName, DoubleArray(data.size){ (it).toDouble() }, data)).displayChart()
 }
-
-class Timer() {
-    private var times = ArrayList<Long>()
-    private var started = false
-    private var startTime: Long = 0
-
-    fun start() {
-        if(!started) {
-            started = true
-            startTime = System.currentTimeMillis()
-        }
-    }
-
-    fun end() {
-        if(started) {
-            started = false
-            times.add(System.currentTimeMillis() - startTime)
-        }
-    }
-
-    fun totalTime() = times.sum()
-}
-
 
 class StockChange(val data: IntArray) {
     val bestBuyDate: Int
@@ -74,7 +69,7 @@ class StockChange(val data: IntArray) {
             else if(data[i-1] > 0 && data[i] <= 0) { // Topp
                 if(sum > bestSum) {
                     bestSum = sum
-                    bestBuy = bottomIndex - 1
+                    bestBuy = if(bottomIndex == 0) 0 else bottomIndex - 1
                     bestSell = i-1
                 }
                 sum = data[i]
--- Oppgaver/src/algoritmerOgDatastrukturer/ovelse2_rekursjon.kt
+++ Oppgaver/src/algoritmerOgDatastrukturer/ovelse2_rekursjon.kt
@@ -1,18 +1,39 @@
 package algoritmerOgDatastrukturer
 
+import java.util.stream.IntStream
+import kotlin.system.measureTimeMillis
 
 
 fun main (args: Array<String>) {
+
+    val maxPowers = 1_000_000L
+
+    val recursivePowerTime = measureTimeMillis {
+        val sum = IntStream.iterate(1, Int::inc).limit(maxPowers).mapToDouble { recursivePower(it, 10) }.sum()
+        println(sum)
+    }
+
+    val recursivePower2Time = measureTimeMillis {
+        val sum = IntStream.iterate(1, Int::inc).limit(maxPowers).mapToDouble { recursivePower2(it, 10) }.sum()
+        println(sum)
+    }
+
+    val javaPowerTime = measureTimeMillis {
+        val sum = IntStream.iterate(1, Int::inc).limit(maxPowers).mapToDouble { Math.pow(it.toDouble(), 10.0) }.sum()
+        println(sum)
+    }
+
     println("""
+
         2.1-1
-        5^2 = ${recursivePower(5, 2)}
-        10^1 = ${recursivePower(10, 1)}
-        25^10 = ${recursivePower(25, 10)}
+        1^10 -> $maxPowers^10
+        tid: $recursivePowerTime ms
 
         2.2-1
-        5^2 = ${recursivePower2(5, 2)}
-        10^1 = ${recursivePower2(10, 1)}
-        25^10 = ${recursivePower2(25, 10)}
+        1^10 -> $maxPowers^10
+        tid: $recursivePower2Time ms
+
+        Java: Math.pow tid: $javaPowerTime ms
     """.trimIndent())
 }
 
@@ -27,7 +48,7 @@ fun recursivePower2 (number: Number, pow: Int) : Double { //2.2-1
     val numberDouble = number.toDouble()
     return when {
         pow == 0 -> 1.0
-        pow % 2 == 0 -> numberDouble * recursivePower2(numberDouble * numberDouble, pow / 2)
-        else -> recursivePower2(numberDouble * numberDouble, (pow - 1) / 2)
+        pow % 2 != 0 -> numberDouble * recursivePower2(numberDouble * numberDouble, (pow-1) / 2)
+        else -> recursivePower2(numberDouble * numberDouble, pow / 2)
     }
 }

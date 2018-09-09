e5e81b4b2dbfff660b98ef9e5673539af32ae468
Sveinung Øverland <sveinungskole@gmail.com>
2018-09-10 01:09:42 +0200

    ghjlvu

--- Oppgaver/src/algoritmerOgDatastrukturer/ovelse3_quicksort.kt
+++ Oppgaver/src/algoritmerOgDatastrukturer/ovelse3_quicksort.kt
@@ -1,54 +1,78 @@
 package algoritmerOgDatastrukturer
 
+import kotlinx.coroutines.experimental.launch
+import java.lang.Exception
 import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 import java.util.stream.IntStream
+import java.util.stream.LongStream
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    val limit = 100_000_000L
-    val testArray = Random().ints().filter { it > 0 }.limit(limit).toArray() //intArrayOf(7,45,2,3,65,78,2,12,4,7,4,45,8,5,34,23,4,67,78,654,4,5456,453,2,5,34,3,34,6,6,213)
+    /*
+        Ta tiden på sortering av:
+        - Tabell med helt tilfeldige data
+        - Tabell med mange duplikater, f.eks hvor annenhvert element er likt.
+        - Tabell som er sortert fra før, da dette er et vanlig spesialtilfelle
 
-    var singlePivotTest: SinglePivot? = null
-    var dualPivotTest: DualPivot? = null
+        Sjekk for korrekt sortering
+     */
 
-    measureTimeMillis {
-        singlePivotTest = SinglePivot(testArray.copyOf())
-    }.also { println("Single pivot with $limit ints: $it ms") }
+    LongStream.of(10_000_000L, 100_000_000L).forEachOrdered { limit ->
+        println("Test with $limit elements")
+        for(i in 1..3) {
+            val (text, array) = when (i) {
+                1 -> "Tabell med tilfeldig data" to Random().ints().filter { it > 0 }.limit(limit).toArray()
+                2 -> "Annenhvert element er 5000" to IntStream.iterate(1, Int::inc).limit(limit).map { if (it % 2 == 0) 5000 else Random().nextInt() }.toArray()
+                3 -> "Tabell med sorterte verdier" to IntStream.iterate(1, Int::inc).limit(limit).map { it * 2 }.toArray()
+                else -> throw Exception("Literally wtf")
+            }
+            println("\t$text")
 
-    measureTimeMillis {
-        dualPivotTest = DualPivot(testArray.copyOf())
-    }.also { println("Dual pivot with $limit ints: $it ms") }
+            val singlePivot = CompletableFuture.supplyAsync {
+                var singlePivot: SinglePivot? = null
+                measureTimeMillis { singlePivot = SinglePivot(array.copyOf()) } to singlePivot!!.isGood
+            }.completeOnTimeout(-1L to false, 30, TimeUnit.MINUTES).thenAccept {
+                when (it.first) {
+                    -1L -> println("\t\tSinglePivot timed out after 30 minutes")
+                    else -> println("\t\tSinglePivot used: ${it.first}ms\tCheck: ${it.second}")
+                }
+            }
 
-    val checkArray = dualPivotTest!!.array.sliceArray(0..100)
-    var check = true
-    for(i in 0 until checkArray.size-1) {
-        if (checkArray[i] > checkArray[i+1]) {
-            check = false
-            println("${checkArray[i]} > ${checkArray[i+1]}")
+            val dualPivot = CompletableFuture.supplyAsync {
+                var dualPivot: DualPivot? = null
+                measureTimeMillis { dualPivot = DualPivot(array.copyOf()) } to dualPivot!!.isGood
+            }.completeOnTimeout(-1L to false, 30, TimeUnit.MINUTES).thenAccept {
+                when (it.first) {
+                    -1L -> println("\t\tDualPivot timed out after 30 minutes")
+                    else -> println("\t\tDualPivot used: ${it.first}ms\tCheck: ${it.second}")
+                }
+            }
+
+            CompletableFuture.allOf(singlePivot, dualPivot).get()
         }
     }
-    println(check)
-    println(Arrays.toString(checkArray))
 }
 
 
 class SinglePivot(val array: IntArray) {
+    val isGood: Boolean by lazy { checkIfCorrect() }
+
     init {
         quicksort(0, array.size-1)
     }
 
-    fun quicksort(lo: Int, hi: Int) { if (lo < hi) {
-        if (hi - lo < 80) insertsort(lo, hi+1)
-        else {
+    private fun quicksort(lo: Int, hi: Int) { if (lo < hi) {
+        //if (hi - lo < 30) insertsort(lo, hi+1)
+        //else {
             val p = partition(lo, hi)
-            //launch { quicksort(lo, p-1) }
             quicksort(lo, p-1)
-            //launch { quicksort(p+1, hi) }
             quicksort(p+1, hi)
-        }
+        //}
     } }
 
-    fun partition(lo: Int, hi: Int): Int {
+    private fun partition(lo: Int, hi: Int): Int {
         val pivot = array[hi]
         var i = lo
         for (j in lo until hi) {
@@ -61,22 +85,31 @@ class SinglePivot(val array: IntArray) {
         return i
     }
 
-    fun insertsort(lo: Int, hi: Int) {
+    private fun insertsort(lo: Int, hi: Int) {
         for (i in lo+1 until hi) {
             for (j in i downTo 1) {
                 if(array[j-1]>array[j]) {array[j-1]+=array[j]; array[j]=array[j-1]-array[j]; array[j-1]-=array[j]} else break
             }
         }
     }
+
+    private fun checkIfCorrect(): Boolean {
+        for (i in 0 until array.size-1) {
+            if (array[i] > array[i+1]) return false
+        }
+        return true
+    }
 }
 
 class DualPivot(val array: IntArray) {
+    val isGood: Boolean by lazy { checkIfCorrect() }
+
     init {
         quicksort(0, array.size-1)
     }
 
     fun quicksort(lo: Int, hi: Int) { if (lo < hi) {
-        if (hi - lo < 80) insertsort(lo, hi+1)
+        if (hi - lo < 30) insertsort(lo, hi+1)
         else {
             //lp = left pivot, rp = right pivot
             val (lp, rp) = partition(lo, hi)
@@ -129,6 +162,13 @@ class DualPivot(val array: IntArray) {
                 if(array[j-1]>array[j]) {array[j-1]+=array[j]; array[j]=array[j-1]-array[j]; array[j-1]-=array[j]} else break
             }
         }
+    }
+
+    private fun checkIfCorrect(): Boolean {
+        for (i in 0 until array.size-1) {
+            if (array[i] > array[i+1]) return false
+        }
+        return true
     }
 }
 
